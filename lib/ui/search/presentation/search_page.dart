3567280cb136a24ce06838e43a083fac003bcd98
paigupai <44311361+paigupai@users.noreply.github.com>
2024-05-19 16:21:43 +0900

    UI構造作成

---
+++ lib/ui/search/presentation/search_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+///
+/// 検索page
+///
+class SearchPage extends StatelessWidget {
+  const SearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/ui/search_details/presentation/search_details_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+///
+/// 検索詳細page
+///
+class SearchDetailsPage extends StatelessWidget {
+  const SearchDetailsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
