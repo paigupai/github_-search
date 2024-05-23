db640fd9ecd0ca95acf482ced1ed265245e2e494
paigupai <44311361+paigupai@users.noreply.github.com>
2024-05-24 01:31:41 +0900

    アニメーション追加

---
+++ lib/features/menu/presentation/component/animated_wave_background.dart
@@ -0,0 +1,146 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:simple_animations/animation_builder/loop_animation_builder.dart';
+import 'package:simple_animations/animation_builder/mirror_animation_builder.dart';
+import 'package:simple_animations/movie_tween/movie_tween.dart';
+
+/// アニメーション波の背景
+class AnimatedWaveBackground extends StatelessWidget {
+  const AnimatedWaveBackground({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        const Positioned.fill(child: AnimatedBackground()),
+        onBottom(const AnimatedWave(
+          height: 180,
+          speed: 1.0,
+        )),
+        onBottom(const AnimatedWave(
+          height: 120,
+          speed: 0.9,
+          offset: pi,
+        )),
+        onBottom(const AnimatedWave(
+          height: 220,
+          speed: 1.2,
+          offset: pi / 2,
+        )),
+        child,
+      ],
+    );
+  }
+
+  onBottom(Widget child) => Positioned.fill(
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: child,
+        ),
+      );
+}
+
+enum _ColorTween { color1, color2 }
+
+class AnimatedBackground extends StatelessWidget {
+  const AnimatedBackground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final tween = MovieTween()
+      ..tween(
+        _ColorTween.color1,
+        ColorTween(
+            begin: const Color(0xffD38312), end: Colors.lightBlue.shade900),
+        duration: const Duration(seconds: 3),
+      )
+      ..tween(
+        _ColorTween.color2,
+        ColorTween(begin: const Color(0xffA83279), end: Colors.blue.shade600),
+        duration: const Duration(seconds: 3),
+      );
+
+    return MirrorAnimationBuilder<Movie>(
+      tween: tween,
+      duration: tween.duration,
+      builder: (context, value, child) {
+        return Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                value.get<Color>(_ColorTween.color1),
+                value.get<Color>(_ColorTween.color2)
+              ])),
+        );
+      },
+    );
+  }
+}
+
+class AnimatedWave extends StatelessWidget {
+  const AnimatedWave(
+      {super.key,
+      required this.height,
+      required this.speed,
+      this.offset = 0.0});
+
+  final double height;
+  final double speed;
+  final double offset;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      return SizedBox(
+        height: height,
+        width: constraints.biggest.width,
+        child: LoopAnimationBuilder<double>(
+            duration: Duration(milliseconds: (5000 / speed).round()),
+            tween: Tween(begin: 0.0, end: 2 * pi),
+            builder: (context, value, child) {
+              return CustomPaint(
+                foregroundPainter: WaveCurvePainter(value + offset),
+              );
+            }),
+      );
+    });
+  }
+}
+
+class WaveCurvePainter extends CustomPainter {
+  final double value;
+
+  WaveCurvePainter(this.value);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final white = Paint()..color = Colors.white.withAlpha(60);
+    final path = Path();
+
+    final y1 = sin(value);
+    final y2 = sin(value + pi / 2);
+    final y3 = sin(value + pi);
+
+    final startPointY = size.height * (0.5 + 0.4 * y1);
+    final controlPointY = size.height * (0.5 + 0.4 * y2);
+    final endPointY = size.height * (0.5 + 0.4 * y3);
+
+    path.moveTo(size.width * 0, startPointY);
+    path.quadraticBezierTo(
+        size.width * 0.5, controlPointY, size.width, endPointY);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+    canvas.drawPath(path, white);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
